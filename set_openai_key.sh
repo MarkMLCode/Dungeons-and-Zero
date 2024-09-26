e3131acea00a8f81005c106b2b514236332ed628
MarkMLCode <MarkML.VTAI@gmail.com>
2024-09-26 13:47:32 -0400

    Fix

--- set_openai_key.sh
+++ set_openai_key.sh
@@ -2,7 +2,7 @@
 
 read -p "Please enter the full OpenAI API key: " API_KEY
 
-echo "export OPENAI_API_KEY=$API_KEY" >> ~/.bashrc
+echo -e "\nexport OPENAI_API_KEY=$API_KEY" >> ~/.bashrc
 
 source ~/.bashrc
 
